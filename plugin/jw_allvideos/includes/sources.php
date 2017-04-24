a781befe3c5ac22def6c71a9aebe6db910edcaf6
Krikor Boghossian <krikor@nuevvo.com>
2017-04-24 17:25:07 +0300

    Added Facebook videos as the source

--- plugin/jw_allvideos/includes/sources.php
+++ plugin/jw_allvideos/includes/sources.php
@@ -630,4 +630,19 @@ $tagReplace = array(
 </object>
 ",
 
+// Facebook.com
+"facebook" => "
+<div id=\"fb-root\"></div>
+<script>
+(function(d, s, id) { var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.src = \"//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3\"; fjs.parentNode.insertBefore(js, fjs);}(document, 'script', 'facebook-jssdk'));
+</script>
+<div class=\"fb-video\" data-allowfullscreen=\"1\" data-href=\"{SOURCE}\">
+<div class=\"fb-xfbml-parse-ignore\">
+<blockquote cite=\"{SOURCE}\">
+<a href=\"{SOURCE}\"></a>
+</blockquote>
+</div>
+</div>
+",
+
 );
