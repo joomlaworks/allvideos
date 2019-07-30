bfe2b14ef562a840c3d4623b6a385091450987ac
Fotis Evangelou <fevangelou@users.noreply.github.com>
2019-07-30 17:12:11 +0300

    Update Classic & Framed templates

--- plugin/jw_allvideos/tmpl/Framed/default.php
+++ plugin/jw_allvideos/tmpl/Framed/default.php
@@ -13,17 +13,14 @@ defined('_JEXEC') or die('Restricted access');
 ?>
 
 <div class="avPlayerWrapper<?php echo $output->mediaTypeClass; ?>">
-	<div style="width:<?php echo $output->playerWidth; ?>px;" class="avPlayerContainer">
-		<div id="<?php echo $output->playerID; ?>" class="avPlayerBlock">
-			<?php echo $output->player; ?>
-			<?php if ($allowAudioDownloading && $output->mediaType=='audio'): ?>
-			<div class="avDownloadLink">
-				<a target="_blank" href="<?php echo $output->source; ?>">
-					<span><?php echo JText::_('JW_PLG_AV_DOWNLOAD'); ?></span>
-				</a>
-				<span class="hint">(<?php echo JText::_('JW_PLG_AV_DOWNLOAD_HINT'); ?>)</span>
-			</div>
-			<?php endif; ?>
-		</div>
-	</div>
+    <div class="avPlayerContainer"<?php echo $maxwidth; ?>>
+        <div id="<?php echo $output->playerID; ?>" class="avPlayerBlock">
+            <?php echo $output->player; ?>
+        </div>
+        <?php if (($allowVideoDownloading && $output->mediaType == 'video') || ($allowAudioDownloading && $output->mediaType == 'audio')): ?>
+        <div class="avDownloadLink">
+            <a target="_blank" href="<?php echo $output->source; ?>" download><?php echo JText::_('JW_PLG_AV_DOWNLOAD'); ?></a>
+        </div>
+        <?php endif; ?>
+    </div>
 </div>
